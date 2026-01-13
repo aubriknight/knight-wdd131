0229f79dd12c9784038fed15fc59dd6d584db25e
aubriknight <aubriknight515@gmail.com>
2026-01-13 10:09:40 -0700

    dom (week2)

---
+++ week2/dom.js
@@ -0,0 +1,8 @@
+
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+})
+                
