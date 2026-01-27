afe4d41e3c2441562ae20487739fc8be3af6ccf2
aubriknight <aubriknight515@gmail.com>
2026-01-27 10:15:21 -0700

    responsive update

--- week3/responsive.js
+++ week3/responsive.js
@@ -4,6 +4,15 @@ const nav = document.querySelector('nav');
 
 menuBtn.addEventListener('click', function() {
     nav.classList.toggle('show');
+    menuBtn.classList.toggle('change');
 });
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 700) {
+        nav.classList.remove('show')
+        menuBtn.classList.remove('change');
+      }    
+});
+
 
 // debouncing
