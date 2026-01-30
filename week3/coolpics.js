88091b93859cc117af70c17b580712e43ec892b5
aubriknight <aubriknight515@gmail.com>
2026-01-29 20:28:52 -0700

    cool pics pt. 2

--- week3/coolpics.js
+++ week3/coolpics.js
@@ -3,4 +3,41 @@ let nav = document.querySelector("nav")
 
 menuBtn.addEventListener("click", function() {
     nav.classList.toggle("show");
+    menuBtn.classList.toggle('change');
+});
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 700) {
+        nav.classList.remove('show')
+        menuBtn.classList.remove('change');
+      }    
+});
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    if(e.target.tagName != "IMG") {
+        return
+    }
+    const thumbSrc = e.target.src;
+    const fullSrc = thumbSrc.includes('-sm') ? thumbSrc.replace('-sm', '-full') : thumbSrc;
+    modalImage.src = fullSrc;
+    modal.showModal();
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
 });
