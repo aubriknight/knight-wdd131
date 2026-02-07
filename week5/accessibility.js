5d5a8abf097c6aa93d054f670d0cda1718ea7351
aubriknight <aubriknight515@gmail.com>
2026-02-07 10:50:02 -0700

    blog

--- week5/accessibility.js
+++ week5/accessibility.js
@@ -5,3 +5,9 @@ allRatingElems.forEach((elem) => {
     const rating = elem.innerText.length;
     elem.setAttribute('aria-label', `${rating} out of 5 stars`);
 });
+
+// for loops 
+// for(let i = 0; i < spans.length; i += 1)
+//      addStars(spans[i]);
+// ^ don't do this if looping through everything (it is the same as line 4)
+// use if need to iterate a certain way, start at different index, break in the middle of the loop (more control)
