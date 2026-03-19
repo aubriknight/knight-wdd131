4099dc70c98438240c8b280df8f052305c2ca1c3
aubriknight <aubriknight515@gmail.com>
2026-03-19 09:35:32 -0600

    site plan

---
+++ index.js
@@ -0,0 +1,121 @@
+const projects = [
+    {
+        name: "Meta Tags and CSS Selectors",
+        week: "1",
+        imgSrc: "wdd131_logo.png",
+        imgAlt: "Project 1 image",
+        description: ""
+    },
+    {
+        name: "Mission Statement pt. 1",
+        week: "1",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "DOM",
+        week: "2",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Computational Thinking",
+        week: "2",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Responsive CSS",
+        week: "3",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Cool Pics",
+        week: "3",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Responsive Gallery",
+        week: "4",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Accessibility",
+        week: "5",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Blog",
+        week: "5",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Array Methods",
+        week: "6",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Movies",
+        week: "6",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Credit Card Form",
+        week: "7",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Objects",
+        week: "8",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Character Card",
+        week: "8",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Flexbox",
+        week: "9",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Recipe Book",
+        week: "9",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    },
+        {
+        name: "Hiking",
+        week: "10",
+        imgSrc: "",
+        imgAlt: "",
+        description: ""
+    }
+]
